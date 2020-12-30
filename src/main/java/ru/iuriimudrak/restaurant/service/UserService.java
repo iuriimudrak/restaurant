ab7e3d2db844798546bb3b9fbee07b5b0c1b6b97
iuriimudrak <mudrak.yura.1991@gmail.com>
2020-12-31 02:26:58 +0300

    Set base app configuration

---
+++ src/main/java/ru/iuriimudrak/restaurant/service/UserService.java
@@ -0,0 +1,14 @@
+package ru.iuriimudrak.restaurant.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserService implements UserDetailsService {
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+}
