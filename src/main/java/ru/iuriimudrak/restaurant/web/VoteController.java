768b2635b185c221ac46fd4ebb0793815ab90f24
iuriimudrak <mudrak.yura.1991@gmail.com>
2021-01-06 01:44:35 +0300

    set VoteController and VoteControllerTest

---
+++ src/main/java/ru/iuriimudrak/restaurant/web/VoteController.java
@@ -0,0 +1,43 @@
+package ru.iuriimudrak.restaurant.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.iuriimudrak.restaurant.model.Vote;
+import ru.iuriimudrak.restaurant.service.VoteService;
+
+import java.net.URI;
+
+import static ru.iuriimudrak.restaurant.web.SecurityUtil.authUserId;
+
+@RestController
+@RequestMapping(value = VoteController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class VoteController {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	protected static final String REST_URL = "/votes";
+
+	private final VoteService voteService;
+
+	@Autowired
+	public VoteController(VoteService voteService) {
+		this.voteService = voteService;
+	}
+
+	@PostMapping
+	public ResponseEntity<Vote> setVote(@RequestParam int restaurantId) {
+		log.info("Set vote for restaurant id= {}", restaurantId);
+		Vote created = voteService.setVote(authUserId(), restaurantId);
+		URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+																											.path(REST_URL + "/{id}")
+																											.buildAndExpand(created.getId()).toUri();
+		return ResponseEntity.created(uriOfNewResource).body(created);
+	}
+}
---
+++ src/test/java/ru/iuriimudrak/restaurant/web/VoteControllerTest.java
@@ -0,0 +1,24 @@
+package ru.iuriimudrak.restaurant.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.iuriimudrak.restaurant.RestaurantTestData.RESTAURANT_1_ID;
+import static ru.iuriimudrak.restaurant.TestUtil.userHttpBasic;
+import static ru.iuriimudrak.restaurant.UserTestData.USER;
+import static ru.iuriimudrak.restaurant.web.VoteController.REST_URL;
+
+class VoteControllerTest extends AbstractControllerTest {
+
+	@Test
+	void setVote() throws Exception {
+		perform(MockMvcRequestBuilders.post(REST_URL)
+																	.param("restaurantId", String.valueOf(RESTAURANT_1_ID))
+																	.with(userHttpBasic(USER))
+																	.contentType(MediaType.APPLICATION_JSON)
+																	.content(""))
+																	.andExpect(status().isCreated());
+	}
+}
